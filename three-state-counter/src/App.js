2cc9a1124139afd1e93413036d32683e0eb8e5c2
The NOS <saurabhyadav311998@gmail.com>
2022-05-16 14:02:56 +0530

    Tictactoe-Start

--- three-state-counter/src/App.js
+++ three-state-counter/src/App.js
@@ -1,20 +1,19 @@
 import React,{useState} from 'react';
-
 import "./App.css";
 
 function App(){
-  const [count,setCount] = useState(0)
+  const [count,setCount] = useState(0) //this is the default value
 
 
   return (
     <div className='App'>
       <header>
-        <h1>Counter app using State/Hook</h1>
+        <h1>Counter app using State/Hooks</h1>
       </header>
       <h2>Current Value of count is {count}</h2>
-      <button>Reset Counter</button>
-      <button>Increase Counter</button>
-      <button>Decrease Counter</button>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => setCount(count+1)}>Increase Counter</button>
+      <button onClick={() => setCount(count-1)}>Decrease Counter</button>
     </div>
   );
 }
